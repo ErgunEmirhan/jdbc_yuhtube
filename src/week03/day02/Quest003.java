3e94f5468e6c4db11ffb97d5de7048bb6e4802a8
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-07-09 19:44:03 +0300

    week03 day02 Quest003 different example added.

--- src/week03/day02/Quest003.java
+++ src/week03/day02/Quest003.java
@@ -25,3 +25,22 @@ public class Quest003 {
 		System.out.println("max deger: "+ max+ " max2 deger: "+ max2);
 	}
 }
+
+/* Hüseyin Tatar'ın çözümü
+public static void main(String[] args) {
+	int[] numbers = {1300, 135, 125, -256, 16, 1258, 81, 14};
+	int max1 = Integer.MIN_VALUE;
+	int max2 = Integer.MIN_VALUE;
+	
+	for(int number : numbers){
+		if(number>max1){
+			max2=max1;
+			max1=number;
+		}else if(number > max2 && number != max1){
+			max2=number;
+		}
+	}
+	System.out.println("Dizideki en buyuk ikinci sayi: " + max2);
+}
+}
+*/
