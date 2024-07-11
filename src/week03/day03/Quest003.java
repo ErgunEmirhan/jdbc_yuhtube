48ec39fe69e609821f0ff018e704d2a81d65cfd4
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-07-11 14:44:33 +0300

    week03 day03 Quest003 modified.

--- src/week03/day03/Quest003.java
+++ src/week03/day03/Quest003.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 3- int[][] matris = { { 56, 23, 678, 231, 5 }, { 234, 21, 78, 26, 6 }, { 654,
 33, 32, 67, 2 }, { 189, 35, 56, 89, 8 } };
 Yukarıdaki 2 boyutlu array'i tek boyutlu bir array olarak yazdıran metot.
-* */
+ */
 public class Quest003 {
 	public static void main(String[] args) {
 		int[][] twoDimensionArr = {
@@ -19,7 +19,12 @@ public class Quest003 {
 	}
 	
 	private static int[] twoDimensionArrToOneDimensionArr(int[][] twoDimArr){
-		int [] oneDimArr = new int[twoDimArr.length*twoDimArr[0].length];
+//		int [] oneDimArr = new int[twoDimArr.length*twoDimArr[0].length]; //asitmetrik matrix'de çalışmaz.
+		int size = 0;
+		for (int i = 0; i < twoDimArr.length; i++){
+			size += twoDimArr[i].length;
+		}
+		int [] oneDimArr = new int[size];
 		int counter = 0;
 		for (int[]row:twoDimArr){
 			for (int item : row){
